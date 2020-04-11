e80aa46ed528aee83f53152df205008153b93d31
luoxue32328 <424293216@qq.com>
2020-04-12 01:45:10 +0800

    disanci

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("我想你了");
-        System.out.println("nimabi");
+        System.out.println("nimabi1");
     }
 }
